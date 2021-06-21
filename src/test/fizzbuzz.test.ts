024823be0b4645e509efc980bd77103b75391977
Javier Chacana <javier.chacana@codurance.com>
2021-06-21 22:36:24 +0200

    RED: new failing test for fizz case 9

--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -21,5 +21,6 @@ describe('Fizz Buzz', () => {
     it('should print `fizz` when getting multiples of 3', () => {
         expect(fizzbuzzOf(3)).toBe('fizz')
         expect(fizzbuzzOf(6)).toBe('fizz')
+        expect(fizzbuzzOf(9)).toBe('fizz')
     })
 })
