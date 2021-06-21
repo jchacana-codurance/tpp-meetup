683d77d3d2d42c97f4187d8dc9bb854a26b29898
Javier Chacana <javier.chacana@codurance.com>
2021-06-21 22:47:52 +0200

    REFACTOR: Removed duplication due to rule of 3

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,7 +1,5 @@
 export function fizzbuzzOf(number: number) {
-    if( number === 20) return 'buzz'
-    if( number === 10) return 'buzz'
-    if( number === 5) return 'buzz'
+    if (number % 5 === 0) return 'buzz'
     if( number % 3 === 0) return 'fizz'
     return number.toString()
 }
