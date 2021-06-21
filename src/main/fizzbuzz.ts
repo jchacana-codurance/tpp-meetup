1ce9551cbd1b381ba132b5c95efc2f9c2a930e89
Javier Chacana <javier.chacana@codurance.com>
2021-06-21 22:46:43 +0200

    GREEN: passing buzz case 20 with minimum (?) code

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export function fizzbuzzOf(number: number) {
+    if( number === 20) return 'buzz'
     if( number === 10) return 'buzz'
     if( number === 5) return 'buzz'
     if( number % 3 === 0) return 'fizz'
