a480d077f22513c81cc56d9f810359995c742f8f
Javier Chacana <javier.chacana@codurance.com>
2021-06-22 23:50:29 +0200

    RED: failing test case for number TWO. Moving into the dimension of symbol I

--- src/test/roman-tpp.test.ts
+++ src/test/roman-tpp.test.ts
@@ -8,4 +8,8 @@ describe('Roman Numerals', () => {
     it("should return 'I' when passing 1", () => {
         expect(romanConverter(1)).toBe('I')
     })
+
+    it("should return 'II' when passing 2", () => {
+        expect(romanConverter(2)).toBe('II')
+    })
 })
