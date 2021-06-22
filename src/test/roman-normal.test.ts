875ccaf064586e1f78fd40a29eab1f2e6b49ea3f
Javier Chacana <javier.chacana@codurance.com>
2021-06-22 22:24:29 +0200

    RED: new failing test for case ONE

--- src/test/roman-normal.test.ts
+++ src/test/roman-normal.test.ts
@@ -4,4 +4,8 @@ describe('Roman Numerals', () => {
     it('should return `` when passing 0', () => {
         expect(romanConverter(0)).toBe('')
     })
+
+    it('should return `I` when passing 1', () => {
+        expect(romanConverter(1)).toBe('I')
+    })
 })
