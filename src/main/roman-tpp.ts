03e2a03261526ed5bd292f99b3ecf58babcc7dc4
Javier Chacana <javier.chacana@codurance.com>
2021-07-04 20:06:39 +0200

    REFACTOR: Applied transformation (statement -> recursion)

--- src/main/roman-tpp.ts
+++ src/main/roman-tpp.ts
@@ -1,6 +1,6 @@
 export function romanConverter(number: number): string {
     if(number === 4)
-        return 'IV';
+        return 'IV' + romanConverter(0);
     if(number >= 1)
         return 'I' + romanConverter(number - 1);
     return '';
