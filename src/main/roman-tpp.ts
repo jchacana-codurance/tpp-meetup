73dcb4807291aeff89c23ac562a2dcb41dfbf42f
Javier Chacana <javier.chacana@codurance.com>
2021-07-04 20:07:44 +0200

    REFACTOR: Applied (constant -> scalar) transformation

--- src/main/roman-tpp.ts
+++ src/main/roman-tpp.ts
@@ -1,6 +1,6 @@
 export function romanConverter(number: number): string {
     if(number === 4)
-        return 'IV' + romanConverter(0);
+        return 'IV' + romanConverter(number - 4);
     if(number >= 1)
         return 'I' + romanConverter(number - 1);
     return '';
