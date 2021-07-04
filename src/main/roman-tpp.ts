3dafccb40178945fef08af11b44e4aac08846cb5
Javier Chacana <javier.chacana@codurance.com>
2021-07-04 20:13:53 +0200

    REFACTOR: Applied (constant -> scalar) and (scalar -> array) transformations

--- src/main/roman-tpp.ts
+++ src/main/roman-tpp.ts
@@ -1,9 +1,10 @@
+const symbols = ['', 'I', 'IV', 'V']
 export function romanConverter(number: number): string {
     if(number >= 5)
-        return 'V' + romanConverter(number - 5);
+        return symbols[3] + romanConverter(number - 5);
     if(number >= 4)
-        return 'IV' + romanConverter(number - 4);
+        return symbols[2] + romanConverter(number - 4);
     if(number >= 1)
-        return 'I' + romanConverter(number - 1);
-    return '';
+        return symbols[1] + romanConverter(number - 1);
+    return symbols[0];
 }
