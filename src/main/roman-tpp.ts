e6355c0feb4c896dd1298674878c72b9b2524f8e
Javier Chacana <javier.chacana@codurance.com>
2021-07-04 20:11:16 +0200

    GREEN: Applied IF transformation. Duplication and pattern spotted

--- src/main/roman-tpp.ts
+++ src/main/roman-tpp.ts
@@ -1,5 +1,7 @@
 export function romanConverter(number: number): string {
-    if(number === 4)
+    if(number >= 5)
+        return 'V' + romanConverter(number - 5);
+    if(number >= 4)
         return 'IV' + romanConverter(number - 4);
     if(number >= 1)
         return 'I' + romanConverter(number - 1);
