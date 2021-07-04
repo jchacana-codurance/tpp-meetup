ecdc01d091d0d6183b6149380dfd46fbf0c09b01
Javier Chacana <javier.chacana@codurance.com>
2021-07-04 20:05:37 +0200

    GREEN: Best possible implementation so far, applied IF transformation

--- src/main/roman-tpp.ts
+++ src/main/roman-tpp.ts
@@ -1,4 +1,6 @@
 export function romanConverter(number: number): string {
+    if(number === 4)
+        return 'IV';
     if(number >= 1)
         return 'I' + romanConverter(number - 1);
     return '';
