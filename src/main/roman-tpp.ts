2bdef36f645e593a8dd3f6e07981753b71eaa0c0
Javier Chacana <javier.chacana@codurance.com>
2021-07-04 20:16:02 +0200

    REFACTOR: Applied (array -> container) transformation. Improved readability at the same time. Possible patterns spotted

--- src/main/roman-tpp.ts
+++ src/main/roman-tpp.ts
@@ -1,9 +1,14 @@
-const symbols = ['', 'I', 'IV', 'V']
+const symbols: Record<number, string> = {
+    0: '',
+    1: 'I',
+    4: 'IV',
+    5: 'V'
+}
 export function romanConverter(number: number): string {
     if(number >= 5)
-        return symbols[3] + romanConverter(number - 5);
+        return symbols[5] + romanConverter(number - 5);
     if(number >= 4)
-        return symbols[2] + romanConverter(number - 4);
+        return symbols[4] + romanConverter(number - 4);
     if(number >= 1)
         return symbols[1] + romanConverter(number - 1);
     return symbols[0];
