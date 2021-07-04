e3bdce21608c662d649106e76cd2b738af16197f
Javier Chacana <javier.chacana@codurance.com>
2021-07-04 20:45:22 +0200

    REFACTOR: parameterized tests to remove duplication

--- src/test/roman-tpp.test.ts
+++ src/test/roman-tpp.test.ts
@@ -1,27 +1,15 @@
 import {romanConverter} from "../main/roman-tpp";
 
 describe('Roman Numerals', () => {
-    it("should return '' when passing 0", () => {
-        expect(romanConverter(0)).toBe('')
-    })
-
-    it("should return 'I' when passing 1", () => {
-        expect(romanConverter(1)).toBe('I')
-    })
-
-    it("should return 'II' when passing 2", () => {
-        expect(romanConverter(2)).toBe('II')
-    })
-
-    it("should return 'III' when passing 3", () => {
-        expect(romanConverter(3)).toBe('III')
-    })
-
-    it("should return 'IV' when passing 4", () => {
-        expect(romanConverter(4)).toBe('IV')
-    })
-
-    it("should return 'V' when passing 5", () => {
-        expect(romanConverter(5)).toBe('V')
+    it.each([
+        ["", 0],
+        ["I", 1],
+        ["II", 2],
+        ["III", 3],
+        ["IV", 4],
+        ["V", 5],
+        ]
+    )("should return '%s' when passing %i", (expected: string, arg: number) => {
+        expect(romanConverter(arg)).toBe(expected)
     })
 })
